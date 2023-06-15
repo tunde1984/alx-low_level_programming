940dec8d27baeafc9f2337c8550c14d65f410598
Tunde <popoolaatunde@gmail.com>
2023-06-15 14:43:49 -0700

    commit in to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+/**
+ * main - printing a line with putfunction
+ * Return: 0
+ */
 int main(void)
 {
-/**
- * this put function
- * the return is always (0)
- */
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
