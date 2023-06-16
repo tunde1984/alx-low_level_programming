e6691bf989691a7e145b7f9c52d2eeb403395810
Tunde <popoolaatunde@gmail.com>
2023-06-15 18:14:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <unstd.h>
+#include <unistd.h>
+
 /**
  * main - entry point
  * Return: 1
  */
-{
 int main(void)
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
+
