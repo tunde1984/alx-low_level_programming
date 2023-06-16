cef302708c6fca16a3a6da2eb3bdb515e4418e21
Tunde <popoolaatunde@gmail.com>
2023-06-15 17:26:02 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unstd.h>
+/**
+ * main - entry point
+ * Return: 1
+ */
+{
+int main(void)
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
