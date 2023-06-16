21a8c2fa07bda6adb3bf839745acf513ff8e75d3
Tunde <popoolaatunde@gmail.com>
2023-06-16 15:40:51 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is 0\n", n, k);
 }
 else
 {
-printf("Last digit of %d is %d and less than 6 and not 0\n", n, k);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
 }
 return (0);
 }
