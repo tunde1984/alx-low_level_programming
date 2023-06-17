0c3fc50b40bd08b310fb7d9c02ea34c02b43069c
Tunde <popoolaatunde@gmail.com>
2023-06-16 17:28:11 -0700

    commit 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Description: its print out numbers from base 10 to 0
+ * Return: 0
+ */
+int main(void)
+{
+putchar('0');
+putchar('1');
+putchar('2');
+putchar('3');
+putchar('4');
+putchar('5');
+putchar('6');
+putchar('7');
+putchar('8');
+putchar('9');
+putchar('\n');
+return (0);
+}
