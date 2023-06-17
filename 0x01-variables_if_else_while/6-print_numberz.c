6b54931e5d52f773c1c7edeacbcf72d0c898f2a5
Tunde <popoolaatunde@gmail.com>
2023-06-16 17:47:37 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,16 +6,11 @@
  */
 int main(void)
 {
-putchar('0');
-putchar('1');
-putchar('2');
-putchar('3');
-putchar('4');
-putchar('5');
-putchar('6');
-putchar('7');
-putchar('8');
-putchar('9');
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n + 48);
+}
 putchar('\n');
 return (0);
 }
