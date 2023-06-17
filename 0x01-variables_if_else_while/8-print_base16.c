8b5e8656b058b4c709f17f4795209667365db500
Tunde <popoolaatunde@gmail.com>
2023-06-16 18:32:32 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Description: this print number 16 in lower case
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+int m;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (m = 97; n <= 102; m++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
