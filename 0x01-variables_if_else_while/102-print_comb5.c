1c72397aeb9c83b2f460fd266882fdfd7af07de2
Tunde <popoolaatunde@gmail.com>
2023-06-19 07:38:54 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,20 +7,31 @@
  */
 int main(void)
 {
+
 int num1, num2;
+
+int firstOutput = 1;
+
 for (num1 = 0; num1 <= 99; num1++)
 {
 for (num2 = num1; num2 <= 99; num2++)
 {
+if (num2 != num1)
+{
+if (!firstOutput)
+{
+putchar(',');
+putchar(' ');
+}
+else
+{
+firstOutput = 0;
+}
 putchar('0' + (num1 / 10));
 putchar('0' + (num1 % 10));
 putchar(' ');
 putchar('0' + (num2 / 10));
 putchar('0' + (num2 % 10));
-if (num1 != 99 || num2 != 99)
-{
-putchar(',');
-putchar(' ');
 }
 }
 }
