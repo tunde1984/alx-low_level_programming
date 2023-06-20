bc9d3e9a37db5a32ecb71c9bb3e5603733f26489
Tunde <popoolaatunde@gmail.com>
2023-06-20 14:35:18 -0700

    commit into alhpabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print the lowercase alphabet
+ */
+
+void print_alphabet(void)
+
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+
+	}
+	_putchar('\n');
+
+}
