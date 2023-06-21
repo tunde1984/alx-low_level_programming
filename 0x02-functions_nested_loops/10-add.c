a713ac282b1830b439a07d6c128ffc3a75d82dcf
Tunde <popoolaatunde@gmail.com>
2023-06-21 04:30:22 -0700

    commit in to 11-print

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,11 @@
 
 /**
  * main - adds integers
- * @n1: the first number input
- * @n2: second number input
+ * @n1: the first number to be computed
+ * @n2: the second number to be computed
  * Return: the sum of n1 and n2
  */
+
 int add(int n1, int n2)
 {
 return (n1 + n2);
