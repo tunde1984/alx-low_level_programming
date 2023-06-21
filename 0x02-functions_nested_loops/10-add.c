1ac50f7e7390a8f77747ab09de9b6a7ce68d7cb9
Tunde <popoolaatunde@gmail.com>
2023-06-21 03:27:45 -0700

    commit 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+int add(int a, int b)
 {
-int n;
-
-n = add(89, 9);
-printf("%d\n", n);
+int sum = a + b;
+return sum;
+}
 return (0);
 }
