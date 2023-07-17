c2341bf5fa91ad5b25ae41e7122515adf7d9f513
Tunde <popoolaatunde@gmail.com>
2023-07-17 10:58:19 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
+
